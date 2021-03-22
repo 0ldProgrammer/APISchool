43923066b38b7a3f11938aa9c95f20ab48d72e30
OldProgrammer <53154671+0ldProgrammer@users.noreply.github.com>
2021-03-22 21:10:38 +0100

    Update connect_and_create_interface.py

--- modules/connect_and_create_interface.py
+++ modules/connect_and_create_interface.py
@@ -14,11 +14,11 @@ def get_id_from_db():
       We connect to the SQL database
       to make changes. __get_id_from_db()__.
     '''
-    connect_db = mysql.connector.connect(user='d', password='d', host='d', database="d")
+    connect_db = mysql.connector.connect(user='x', password='x', host='x', database="x")
     request_db = connect_db.cursor()
     request_db.execute("SELECT `id` FROM pentest_network")
     result_req = request_db.fetchall()
-    
+
     for x in result_req:
         number = str(x)
         id = number.strip("(,)")
