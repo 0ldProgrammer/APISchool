fa62b750b96099d77819f00a17d23fe7a432efc9
OldProgrammer <53154671+0ldProgrammer@users.noreply.github.com>
2021-04-07 19:41:43 +0200

    Update start.py

--- modules/start.py
+++ modules/start.py
@@ -8,37 +8,17 @@ class IncorrectToStartMachine(Exception):
         def __init__(self, error=None):
                 self.error = error
 
-def IdentityOfMachine(passing_parameter):
-        '''
-                This function tests the
-                value of the function __IdentityOfMachine()__.
-        '''
-        if(len(passing_parameter) != 4):
-                raise IncorrectToStartMachine("Incorrect to start machine, sorry!")
-
-        if(passing_parameter[3] == "1"):
-                bool_data = True
-        elif(passing_parameter[3] == "2"):
-                bool_data = False
-
-        return bool_data
-
-def StartMachinePCT(number_of_machine, user_id, random_numbers):
+def StartMachinePCT(number_of_machine, user_id):
         '''
                 This function will start the
                 containers and virtual machines. __StartMachinePCT()__.
         '''
 
         network_setting = f"name=eth0,bridge=vmbr2,ip=172.30.10.{user_id}/24,gw=172.30.10.254,type=veth"
-        number_of_machine = str(int(200 + number_of_machine))
+        user_machine = str(int(200 + int(user_id)))
 
-        send_command    = [["pct", "unlock", str(number_of_machine)], ["pct", "clone", str(200 + number_of_machine), str(random_numbers)], ["pct", "set", str(random_numbers), "-net0", network_setting], ["pct", "start", str(random_numbers)]]
+        send_command    = [["pct", "unlock", str(number_of_machine)], ["pct", "clone", str(number_of_machine), str(user_machine)], ["pct", "set", str(user_machine), "-net0", network_setting], ["pct", "snapshot", user_machine, "backup"], ["pct", "start", str(user_machine)]]
 
         for j in send_command:
                 subprocess.run(j)
         return
-
-
-def StartMachineQM(number_of_machine):
-        if(type(number_of_machine) != str):
-                raise IncorrectToStartMachine("Incorrect to start machine, sorry!")
