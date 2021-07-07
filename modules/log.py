800cdbbb16330e3b318f8c2f096ef2d761e603f6
OldProgrammer <53154671+0ldProgrammer@users.noreply.github.com>
2021-07-07 20:48:38 +0200

    Update log.py

--- modules/log.py
+++ modules/log.py
@@ -4,7 +4,30 @@ import os
 import re
 import json
 import subprocess
+import mysql.connector
 from datetime import datetime
+
+def recover_username(id_of_user):
+        """
+                This function will make it possible to retrieve the name of the
+                user using the identifier and this contacts the SQL database.
+
+                Function: __recover_username__().
+        """
+        contact_mysql = mysql.connector.connect(user='x', password='x',
+                              host='x',
+                              database='x')
+
+        send_command = contact_mysql.cursor()
+        query_request = ("SELECT user_login FROM wp_users WHERE ID=" + id_of_user)
+        send_command.execute(query_request)
+
+        list_append = []
+
+        for user_pass in send_command:
+                for j in user_pass:
+                        list_append.append(j)
+        return list_append[0]
 
 def recover_name_of_machine(argument_of_machine):
         """
@@ -37,7 +60,7 @@ def log_time(id_of_user, id_of_machine):
         update_time = path_of_variable + "/" + str(id_of_user) + "_" + str(datetime.now().strftime('%m-%d-%Y-%H-%M-%S')) + ".json"
         date_time   = datetime.now().strftime('%H:%M:%S')
         with open(update_time, "w") as create_json_file:
-                data = {'information': [{'id_user': str(id_of_user), 'id_machine': id_of_machine,
+                data = {'information': [{'id_user': str(id_of_user), 'username': recover_username(id_of_user), 'id_machine': id_of_machine,
                         'name_machine': str(recover_name_of_machine(id_of_machine)),
                         "time_start":str(date_time), 'status':'on', "time_end":"None"}]}
                 create_json_file.write(json.dumps(data, indent=4, sort_keys=True))
@@ -63,3 +86,5 @@ def log_stop(id_of_user, path_file):
 
         with open(path_file + str(id_of_user) + "/" + json_path_file[-1], "w") as overwrite_file:
                 overwrite_file.write(json.dumps(json_object, indent=4, sort_keys=True))
+                
+ 
