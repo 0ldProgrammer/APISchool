64b11bba48975101fe58ffd06fad57d458a2d67c
OldProgrammer <53154671+0ldProgrammer@users.noreply.github.com>
2021-07-11 16:27:05 +0200

    Update start.py

--- modules/start.py
+++ modules/start.py
@@ -17,8 +17,13 @@ def StartMachinePCT(number_of_machine, user_id):
         network_setting = f"name=eth0,bridge=vmbr2,ip=172.30.10.{user_id}/24,gw=172.30.10.254,type=veth"
         user_machine = str(int(200 + int(user_id)))
 
-        send_command    = [["pct", "unlock", str(number_of_machine)], ["pct", "clone", str(number_of_machine), str(user_machine)], ["pct", "set", str(user_machine), "-net0", network_setting], ["pct", "snapshot", user_machine, "backup"], ["pct", "start", str(user_machine)]]
+        send_command    = [["pct", "unlock", str(number_of_machine)], ["pct", "clone", str(number_of_machine), str(user_machine), "--pool", "Users_Pentest"], ["pct", "set", str(user_machine), "-net0", network_setting], ["pct", "start", str(user_machine)]]
 
         for j in send_command:
                 subprocess.run(j)
+
         return
+
+def StartMachineQM(number_of_machine):
+        if(type(number_of_machine) != str):
+                raise IncorrectToStartMachine("Incorrect to start machine, sorry!")
