b5a12ca6524ac4002dc91595b60c6cd27999e1f8
OldProgrammer <53154671+0ldProgrammer@users.noreply.github.com>
2021-07-11 20:09:50 +0200

    Update restore.py

--- modules/restore.py
+++ modules/restore.py
@@ -4,7 +4,7 @@ import subprocess
 
 def RestoreMachinePCT(id_user):
         id_user = 200 + int(id_user)
-        pct_exec = [["pct", "stop", str(id_user)], ["pct", "rollback", str(id_user), "backup"], ["pct", "start", str(id_user)]]
+        pct_exec = [["/usr/sbin/pct", "stop", str(id_user)], ["/usr/sbin/pct", "rollback", str(id_user), "backup"], ["/usr/sbin/pct", "start", str(id_user)]]
 
         for j in pct_exec:
                 subprocess.run(j)
