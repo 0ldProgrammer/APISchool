cf2909576f2b2baf08939b15cf9d6f4cbca665ce
OldProgrammer <53154671+0ldProgrammer@users.noreply.github.com>
2021-07-11 16:25:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,14 +5,16 @@ import os
 import re
 import subprocess
 import time
+import random
+
 from random import randint
 from flask import Flask, request, redirect
 
-from modules import autoshut
 from modules import start
 from modules import restore
 from modules import stop
 from modules import machine
+from modules import log
 
 app = Flask(__name__)
 WEB_DOMAIN = "www.whs.fr"
@@ -25,13 +27,15 @@ def get_url():
         """
         id_vm = request.args.get('vm')
 
-        with open('status/' + request.args.get('user') + ".qf", "w") as f_write:
-                f_write.write(' ')
+        with open('status/' + str(request.args.get('user')) + ".qf", "w") as f_write:
+                f_write.write('pid=' + str(random.randint(5555,9999)) + "\n")
 
         if(int(machine.IdentityOfMachine(id_vm)) == 1):
+                log.log_time(str(request.args.get('user')), str(request.args.get('vm')[:3]))
                 start.StartMachinePCT(request.args.get('vm')[:3], request.args.get('user'))
 
         elif(int(machine.IdentityOfMachine(id_vm)) == 2):
+                log.log_stop(str(request.args.get('user')), '/root/APISchool/log/')
                 stop.StopMachinePCT(request.args.get('user'))
 
         return ""
