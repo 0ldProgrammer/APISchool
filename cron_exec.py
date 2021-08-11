27e620c5b2d6ea49c65288c0c26d8231d7cc9918
OldProgrammer <53154671+0ldProgrammer@users.noreply.github.com>
2021-08-12 01:34:15 +0200

    Create cron_exec.py

---
+++ cron_exec.py
@@ -0,0 +1,69 @@
+#coding:utf-8
+
+import phpserialize
+import mysql.connector
+import sys
+import re
+
+if(len(sys.argv) < 2):
+  sys.exit('miss argument roles!')
+
+def check_checkpoint(name_of_argument):
+        checkpoint_re = re.findall('checkpoint0[1-4]', name_of_argument)
+        if(checkpoint_re != []):
+                return True
+        return False
+def update_values():
+        contact_mysql = mysql.connector.connect(user='wordpress', password='fdd12f34d9996d851cfb793adb4b4ed1',                host='127.0.0.1', database='wordpress')
+        send_command = contact_mysql.cursor()
+        query_request = ("SELECT wp_users.ID, wp_users.user_login, wp_usermeta.meta_value FROM wp_users INNER JOIN wp_usermeta ON wp_users.ID = wp_usermeta.user_id WHERE wp_usermeta.meta_key = 'wp_capabilities';")
+        send_command.execute(query_request)
+
+        list_append = {}
+        for j in send_command.fetchall():
+                id, user, roles = j
+                if("administrator" not in roles):
+                        dict = phpserialize.unserialize(bytes(roles, encoding='utf8'))
+                        argument_called = "um_" + sys.argv[1]
+                        dict.update({argument_called: True})
+                        list_append[id] = phpserialize.serialize(dict)
+
+        return list_append
+
+                return True
+        return False
+
+def update_values():
+        contact_mysql = mysql.connector.connect(user='wordpress', password='x',
+                host='x', database='x')
+        send_command = contact_mysql.cursor()
+        query_request = ("SELECT wp_users.ID, wp_users.user_login, wp_usermeta.meta_value FROM wp_users INNER JOIN wp_usermeta ON wp_users.ID = wp_usermeta.user_id WHERE wp_usermeta.meta_key = 'wp_capabilities';")        send_command.execute(query_request)
+
+        list_append = {}
+
+        for j in send_command.fetchall():
+                id, user, roles = j
+                if("administrator" not in roles):
+                        dict = phpserialize.unserialize(bytes(roles, encoding='utf8'))
+                        argument_called = "um_" + sys.argv[1]
+                        dict.update({argument_called: True})
+                        list_append[id] = phpserialize.serialize(dict)
+
+        return list_append
+
+class ExceptionErrorMySQL(Exception):
+        def __init__(self, error=None):
+                self.error = error
+
+def send_mysql():
+        var = update_values()
+        for j in var:
+                contact_mysql = mysql.connector.connect(user='wordpress', password='fdd12f34d9996d851cfb793adb4b4ed1', host='127.0.0.1', database='wordpress')
+                send_command = contact_mysql.cursor()
+
+                query_request = ("UPDATE wp_usermeta SET meta_value = '%s' WHERE user_id = '%s' && meta_key = 'wp_capabilities';" %(var[j].decode('utf8'), str(j)))
+                send_command.execute(query_request)
+                contact_mysql.commit()
+
+if __name__ == "__main__":
+        send_mysql()
